bd83d80511ae39a45298b1990972d371b19be518
Silurana <silurana330@gmail.com>
2026-07-27 21:38:02 +0530

    Arithmatic Operator

---
+++ arithmaticOpe.java
@@ -0,0 +1,22 @@
+public class arithmaticOpe {
+    public static void main(String args[]) {
+        int a = 10, b = 3;
+
+        // Add
+        System.out.println(a + b);
+
+        // Minus
+        System.out.println(a - b);
+
+        // Mul
+        System.out.println(a * b);
+
+        // Div
+        System.out.println(a / b); // 3
+        System.out.println(a / (float)b); // 3.333
+
+        // Modulo ope
+        System.out.println(a % b);
+        
+    }
+}
