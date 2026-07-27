6e6767d89039144a11cc7e318d3f620efe8ebe54
Silurana <silurana330@gmail.com>
2026-07-28 01:34:46 +0530

    Counts Digit Of a Number

---
+++ CountsDigitOfNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CountsDigitOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int digits = 0;
+
+        while(num > 0) {
+            num /= 10;
+            digits++;
+        }
+        System.out.println(digits);
+    }
+}
