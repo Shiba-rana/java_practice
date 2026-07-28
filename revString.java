6fadbcf588add2ac5a5448ab5a94689d7b27fd61
Silurana <silurana330@gmail.com>
2026-07-28 20:12:09 +0530

    Reverse a String

---
+++ revString.java
@@ -0,0 +1,12 @@
+public class revString {
+    public static void main(String[] args) {
+        String str = "java";
+        String rev = "";
+
+        for(int i = str.length() - 1; i >=0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
