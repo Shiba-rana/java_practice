fb12546faa027345fe057c62e91218aaa1991dd2
Silurana <silurana330@gmail.com>
2026-07-29 14:31:28 +0530

    Square of a number using function

--- squareNumber.java
+++ squareNumber.java
@@ -1,9 +1,13 @@
+import java.util.*;
 class squareNumber {
     public static void main(String[] args) {
-        square(5);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        square(num);
     }
 
     static void square(int num) {
+        
         System.out.println(num*num);
     }
 }
