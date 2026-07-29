e15b17b7467b44e4624ee8481f5708859e0e3a28
Silurana <silurana330@gmail.com>
2026-07-30 01:55:18 +0530

    Two D Array

---
+++ TwoDArray.java
@@ -0,0 +1,18 @@
+public class TwoDArray {
+    public static void main(String[] args) {
+        int [][]a = {
+            {1,2,3},
+            {4,5,6}
+        };
+
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[0][2]);
+        
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+        System.out.println(a[1][2]);
+
+    }
+
+}
