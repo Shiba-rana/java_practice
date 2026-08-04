82b4d6a6f3949dd7db9ea2f1740899dcf1d015a7
Silurana <silurana330@gmail.com>
2026-08-04 22:42:35 +0530

    Student Data

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    int yob;
+    String name;
+    
+    public int check() {
+        return yob;
+    }
+}
---
+++ StudentData.java
@@ -0,0 +1,12 @@
+public class StudentData {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.name = "Shubu";
+        s.yob = 2004;
+
+        System.out.println(s.name + " " + s.yob);
+
+        System.out.println(s.check());
+    }
+}
