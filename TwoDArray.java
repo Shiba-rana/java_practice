0f3003b8798e9f9fdee5a5ee016b3157130cfb1b
Silurana <silurana330@gmail.com>
2026-08-12 15:13:51 +0530

    2D Array Input

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class TwoDArray {
     public static void main(String[] args) {
+
+        // predefind
         int [][]a = {
             {1,2,3},
             {4,5,6}
@@ -12,6 +16,30 @@ public class TwoDArray {
         System.out.println(a[1][0]);
         System.out.println(a[1][1]);
         System.out.println(a[1][2]);
+
+        System.out.println("\n\t");
+
+        // input
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        // print matrix
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
